09971fe16f980a4c04bcfd5f019ba6d544a977ed
Bazsmagister <bosmer007@gmail.com>
2020-09-20 21:19:59 +0200

    up

--- source/_partials/nav.blade.php
+++ source/_partials/nav.blade.php
@@ -1,9 +1,12 @@
-<nav>
-    <ul>
-        <li><a class="{{ $page->selected('') }}" href="/">Home</a></li>
+<nav class="navbar">
+
+    <ul class="menu">
+        <li><a class=" {{ $page->selected('') }}" href="/">Home</a></li>
         <li><a class="{{ $page->selected('about') }}" href="/about">About</a></li>
         <li><a class="{{ $page->selected('contact') }}" href="/contact">Contact</a></li>
         <li><a class="{{ $page->selected('blog') }}" href="/blog">Blog</a></li>
         <li><a class="{{ $page->selected('posts') }}" href="/posts">Posts</a></li>
     </ul>
+
+
 </nav>
